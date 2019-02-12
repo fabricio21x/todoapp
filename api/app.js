ba79ea7562c6cfe14218639e55ede35813ac8e7d
Fabricio Monsalve <fabricio2111@gmail.com>
2019-02-11 21:44:49 -0500

    basic api

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import db from './db/db';
 
 const app = express();
@@ -7,11 +8,11 @@ app.get('/api/v1/list', (req, res) => {
 	res.status(200).send({
 		success: 'true',
 		message: 'ToDo list received successfuly',
-		todos: db
+		data: db
 	});
 });
 
-const PORT = 1411;
+const PORT = 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
